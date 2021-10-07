fb95f31f105ae95677ebaef4f6f390fe343a19cc
Kevin Koffroth <ktkoffroth@gmail.com>
2021-10-07 15:33:26 -0400

    Added keypad.asm, Buffalo, and worked on Tachometer lab

---
+++ keypad.asm
@@ -0,0 +1,129 @@
+ ; Project 1: Keypad Input
+
+; Constants and Utility Subroutines
+DELAY EQU $FFFF
+PORTG EQU $2200
+PORTGDDR EQU $2202
+OUTSTRG EQU $FFC7 ; string output Utility Subroutine
+OUTA    EQU $FFB8 ; char output Utility Subroutine
+Message FCC "Type a character: " ; Store Message
+        FCB $04
+POLLTABLE FCB $FE,$FD,$FB,$F7 ; PORTG polling values
+
+; Variables (KEYTABLE)
+KEYTABLE FCB $00,$00,$00,$00  ; first 16 bytes are the key values
+         FCB $00,$00,$00,$00  ; Last 16 bytes are the ASCII
+         FCB $00,$00,$00,$00
+         FCB $00,$00,$00,$00
+         FCB $30,$34,$38,$43
+         FCB $31,$35,$39,$44
+         FCB $32,$36,$41,$45
+         FCB $33,$37,$42,$46
+
+        ORG $D000
+        LDS #$D500 ; Init SP
+        
+; For telling user to type in characters
+        LDX #Message
+        JSR OUTSTRG
+
+; Main Loop
+MAIN:
+        JSR POLLKEYPAD ; Poll the Keypad
+        LDX #KEYTABLE
+        LDAB #16
+
+CHECKKEYTABLE: ; Check KEYTABLE [0 - 15]
+        LDAA 0,X
+        BEQ PRINTSKIP ; Skip if position is 0
+        LDAA 16,X ; Add 16 to pointer, load char
+        JSR OUTA ; call Utility Subroutine
+        PSHX ; We need X for printing, so Push
+        LDX #Message
+        JSR OUTSTRG
+        PULX ; and Pull
+PRINTSKIP:
+        INX ; increment X to check next position
+        DECB
+        BNE CHECKKEYTABLE ; Loop 16 times, check all positions
+
+       BRA MAIN ; While(true)
+        
+; POLL KEYPAD SUBROUTINE
+POLLKEYPAD:
+        LDAA #$0F   ;SET PORT G
+        STAA PORTGDDR ;FOR I/O
+        LDAB #4
+        LDX #POLLTABLE ; Load POLLTABlE and KEYTABLE pointers
+        LDY #KEYTABLE
+COLUMN:
+        LDAA 0,X       ; Turn off correct column to poll
+        STAA PORTG
+        PSHX           ; Save POLLTABLE pointer, Need X
+ROW:
+        LDAA #0        ; Set value in KEYTABLE to 0 by default
+        STAA 0,Y
+        LDAA #$10  ; Bit Mask 0b00010000
+        ANDA PORTG ; Check first Row Bit
+        BNE SKIP1  ; Skip if input is non-zero (no press) yu
+        INC 0,Y         ; increment button value in KEYTABLE
+        LDX #DELAY      ; Setup DELAY for debounce
+
+DELAY1:                 ; Debouce Loop
+       DEX
+       BNE DELAY1
+SKIP1:
+       INY        ; Move KEYTABLE pointer to next row
+       LDAA #0
+       STAA 0,Y
+       LDAA #$20  ; Bit Mask 0b00100000
+        ANDA PORTG ; Check first Row Bit
+        BNE SKIP2  ; Skip if input is non-zero (no press)
+       INC 0,Y
+       LDX #DELAY
+
+DELAY2:
+       DEX
+       BNE DELAY2
+
+SKIP2:
+       INY
+       LDAA #0
+       STAA 0,Y
+       LDAA #$40 ; Bit Mask 0b01000000
+        ANDA PORTG ; Check first Row Bit
+        BNE SKIP3  ; Skip if input is non-zero (no press)
+       INC 0,Y
+       LDX #DELAY
+
+DELAY3:
+       DEX
+       BNE DELAY3
+
+SKIP3:
+       INY
+       LDAA #0
+       STAA 0,Y
+       LDAA #$80 ; Bit Mask 0b10000000
+        ANDA PORTG ; Check first Row Bit
+        BNE SKIP4  ; Skip if input is non-zero (no press)
+       INC 0,Y
+       LDX #DELAY
+
+DELAY4:
+       DEX
+       BNE DELAY4
+       
+SKIP4:
+        INY ; increment Y, setup KEYTABLE pointer
+            ; for next loop iteration
+
+        PULX
+        INX
+        DECB
+        BNE COLUMN
+        RTS
+; END POLL KEYPAD SUBROUTINE
+
+        SWI
+        END
--- tachometer.asm
+++ tachometer.asm
@@ -3,22 +3,38 @@ PORTA0 EQU $1014 ; PA0 Address, Input Capture Register 0
 TCTL2 EQU $1021 ; Timer Control Register
 TMSK1 EQU $1022 ; Timer Interrupt Mask Register
 TFLG1 EQU $1023 ; Timer Interrupt Flag Register
-ASCII EQU 30 ; 
+PORTG EQU $2200 ; Port G Address 
+PORTGDDR EQU $2202 ; Port G DDR Address
+
+;Init Utility Subroutines
+OUTSTRG EQU $FFC7 ; string output Utility Subroutine
+OUTA    EQU $FFB8 ; char output Utility Subroutine
 
 ; Init Constants
+DELAY EQU $FFFF ; Keypad debounce
 RPMCOUNT EQU 32 ; RPM Sum Count (used for sum loop and average)
+NUMBERS FCB $30,$31,$32,$33,$34,$35,$36,$37,$38,$39 ; 0-9 ASCII Representation
+RPMMSG FCC "Average RPM: " ; Store Message
+       FCB $04
+POLLTABLE FCB $FE,$FD,$FB,$F7 ; PORTG polling values
+KEYTABLE FCB $00,$00,$00,$00  ; first 16 bytes are the key values
+         FCB $00,$00,$00,$00  ; Last 16 bytes are the ASCII
+         FCB $00,$00,$00,$00
+         FCB $00,$00,$00,$00
+         FCB $30,$34,$38,$43
+         FCB $31,$35,$39,$44
+         FCB $32,$36,$41,$45
+         FCB $33,$37,$42,$46
+
 
 ; Init Variables
-T2 FCB $00, $00 ; Previous PA0 value
-T1 FCB $00, $00 ; Current PA0 value
-PWIDTH FCB $00, $00 ; Calculated Pulse Width
-RPMVALUE FCB $00, $00 ; Calculated RPM
-RPMSUM FCB $00, $00 ; Rolling RPM Sum
-RPMAV FCB $00, $00 ; Calculated Average RPM
-NUMBERS FCB $30, $31, $32,
-            $33, $34, $35,
-            $36, $37, $38
-            $39
+T2 FCB $00,$00 ; Previous PA0 value
+T1 FCB $00,$00 ; Current PA0 value
+PWIDTH FCB $00,$00 ; Calculated Pulse Width
+RPMVALUE FCB $00,$00 ; Calculated RPM
+RPMSUM FCB $00,$00 ; Rolling RPM Sum
+COUNT FCB $00 ; counter variable used in multiple places
+RPMAV FCB $00,$00 ; Calculated Average RPM
 
 
 ; Store JMP to interrupt handler at pseudo-vector for IC3
@@ -27,6 +43,7 @@ NUMBERS FCB $30, $31, $32,
 
 ; Main Program Entry
     ORG $D000
+    LDS #$D500 ; Init SP
 
 ;both the I bit has to be cleared and local mask bit has to be set for interrupt to occur, set up during iniitialization
 IC3INIT:
@@ -37,18 +54,35 @@ IC3INIT:
     STAA TFLG1
 
 MAIN:
-    JSR SENSOR; Calculate RPM
+    JSR SENSOR ; Calculate RPMAV
+    
 
-    BRA ; While(true)
+    BRA MAIN ; While(1)
 
 ; Main Subroutines
 
 ; SENSOR main subroutine
 SENSOR:
         WAI ; Wait for next Interrupt Signal
-        JSR PWIDTH ; Calculate PWIDTH
+        JSR CALCPWIDTH ; Calculate PWIDTH
         JSR RPM ; Calculate RPM
-        RTS ; 
+        
+        LDD RPMSUM ; Add RPM to running sum
+        ADDD RPMVALUE
+        STD RPMSUM
+        INC COUNT
+        LDAB RPMCOUNT
+        CMPB COUNT
+        BNE SENSOR ; Branch as long as the count has not reached 32
+
+        LDX RPMCOUNT ; When we've taken 32 sums, divide by 32 and put into RPMAV
+        LDD RPMSUM
+        IDIV
+        STX RPMAV
+
+        CLR COUNT ; Reset COUNT variable to zero
+
+        RTS ; Return
 
 ; KEYPAD main subroutine
 KEYPAD: 
@@ -58,7 +92,7 @@ KEYPAD:
 ; Helper Subroutines
 
 ; Calculate PWIDTH Subroutine
-PWIDTH: 
+CALCPWIDTH: 
         LDD T1
         SUBD T2 ; PWIDTH = T1 - T2
 	    BMI RECALC ; if T2 > T1
@@ -75,6 +109,40 @@ RECALC:
 
 ; Calculate RPM Subroutine
 RPM: 
+        LDD PWIDTH ; Load Delta T
+        LDX #32 ; Common Factor Divide
+        IDIV
+        LDD #62500
+        IDIV ; Result in X
+        STX RPMVALUE
+        RTS
+
+; Convert Average RPM Value to ASCII and print to Buffalo Terminal (PRINT Subroutine)
+PRINT:
+        LDX #RPMMSG
+        JSR OUTSTRG ; Print RPMMSG onto Buffalo Terminal
+        LDX #10 ; bin to decimal conversion
+        LDD RPMAV
+; Calculate remainder and push it's ASCII to the stack
+REMAINDER:
+        LDY NUMBERS
+        IDIV ; Value in X, remainder in D
+        ABY ; Move to Correct ASCII Character
+        LDAB 0,Y ; Load ASCII Character
+        PSHB ; Push the ASCII to the Stack
+        INC COUNT ; Increment Character Count
+        XGDX
+        CPD #0
+        BNE REMAINDER
+; Display ASCII from stack
+DISPLAY:
+        PULA
+        JSR OUTA ; Print Character
+        DEC COUNT
+        BNE DISPLAY
+        CLR COUNT ; Reset COUNT variable to zero
+
+        RTS ; Return
 
 
 ; Interupt Handler, Simply Capture T1
